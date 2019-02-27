b92b8d7f747792b0ff623ed58dd0ec20bb71424c
Jack Walker <jwrc@protonmail.com>
2019-02-27 10:23:55 +1000

    Finished challenges

--- 05_in_array.rb
+++ 05_in_array.rb
@@ -13,11 +13,36 @@
 # Example:
 # "hello", ["hi","howdy","hello"] should return true.
 
-def in_array (needle, haystack)
-  # Your code here
+# Defining method to check validity of string being in array
+# and string being
+def in_array(needle, haystack)
+  (string_in_array(needle, haystack) == true and needle.is_a? String) ? true : false  
 end
 
+def string_in_array(needle, haystack)
+    if haystack.include?(needle)
+      return true
+    end
+  return false
+end
 
 # Beast Mode:
 
-# Redo the challenge but try solving it again in a different way.
+def in_array(needle, haystack)
+  if string_in_array(needle, haystack) == true and needle.is_a? String
+    return true
+  else
+    return false
+  end
+end 
+
+def string_in_array(needle, haystack)
+  haystack.each do |item|
+    if needle == item
+      return true
+    end
+  end
+  return false
+end
+
+# # Redo the challenge but try solving it again in a different way.
