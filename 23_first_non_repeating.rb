7824a6d60783d4cfd868f3d324cd196c36b7d415
Jack Walker <jwrc@protonmail.com>
2019-03-28 12:50:13 +1000

    Alternative solution to 23

--- 23_first_non_repeating.rb
+++ 23_first_non_repeating.rb
@@ -25,18 +25,34 @@
 # as it would have to go over every character. Can you return on the first 
 # non-repeat, without checking every other letter?
 
+# def first_non_repeating(input)
+#     # Your code goes here
+#     # Split string into array of individual characters
+#     input_arr = input.downcase.chars 
+#     # Identify unique characters
+#     input_arr_unique_elements = input_arr.uniq
+#     # Iterate through string array, checking each unique letter and counting it
+#     input_arr_unique_elements.each do |x|
+#         unique_element_count = input_arr.count(x)
+#         if unique_element_count == 1
+#             return x
+#         end
+#     end
+#     return false
+# end
+
 def first_non_repeating(input)
-    # Your code goes here
-    # Split string into array of individual characters
-    input_arr = input.downcase.chars 
-    # Identify unique characters
-    input_arr_unique_elements = input_arr.uniq
-    # Iterate through string array, checking each unique letter and counting it
-    input_arr_unique_elements.each do |x|
-        unique_element_count = input_arr.count(x)
-        if unique_element_count == 1
+    input_arr = input.downcase.chars
+    while input_arr.length > 0 do
+      input_arr.each do |x|
+        element_count = input_arr.count(x)
+        if element_count > 1
+            input_arr.delete(x)
+            break
+        elsif element_count == 1
             return x
         end
+      end
     end
     return false
 end
