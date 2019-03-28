b53194ea1131cfbe247ccf4e7e02bd9ab7bec35e
Jack Walker <jwrc@protonmail.com>
2019-03-29 08:54:15 +1000

    Finished challenge 9

--- 09_vowels.rb
+++ 09_vowels.rb
@@ -12,4 +12,17 @@
 
 def vowels (string)
   # Your code here
+  # String to characters array
+  string_arr = string.chars
+  output_arr = []
+  # Iterate through array, case statement for vowels, add to a vowels_arr
+  string_arr.each do |char|
+    case char
+    when "a", "e", "i", "o", "u"
+      output_arr << char
+    when "A", "E", "I", "O", "U"
+      output_arr << char
+    end
+  end
+  return output_arr
 end
