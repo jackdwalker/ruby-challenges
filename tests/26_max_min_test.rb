f3db90642139d06b7a2ef811a7d9e3482329a21d
Jack Walker <jwrc@protonmail.com>
2019-04-03 08:11:00 +1000

    Unsaved changes?

---
+++ tests/26_max_min_test.rb
@@ -0,0 +1,10 @@
+require 'test/unit'
+require_relative '../26_max_min'
+
+class MaxMinTest < Test::Unit::TestCase
+    def test_all_positive
+    end
+    def test_all_negative
+    end
+    def test_mixed
+end
